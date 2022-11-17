142497a9f31bc97a9b3031560d5cdeec0f987290
Vitka <kanochkin@yandex.ru>
2022-11-17 13:17:18 +0300

    cor.2

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const editButtonElem = document.querySelector('.profile__edit-button');
 const popupElem = document.querySelector('.popup');
 const popupCloseElem = document.querySelector('.popup__close');
 
-
+const formElement = document.querySelector('.popup__form');
 const nameInput = document.querySelector('.popup__text_input_name');
 const jobInput = document.querySelector('.popup__text_input_job');
 const titleElem = document.querySelector('.profile__title');
@@ -29,10 +29,11 @@ popupCloseElem.addEventListener('click', () => {
 
 
 function formSubmitHandler(evt) {
-  evt.preventDefault();  
-  titleElem.textContent = titleElem.value;
-  subtitleElem.textContent = subtitleElem.value;
+  evt.preventDefault();
+  titleElem.textContent = nameInput.value;
+  subtitleElem.textContent = jobInput.value;
+  onClose(popupElem);
 }
-popupElem.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', formSubmitHandler);
 
 
